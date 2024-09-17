e95618298c0d9ec6e127290990bacd943078bcc4
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-17 10:48:13 +0300

    services and tests addings

--- src/test/kotlin/com/hibob/academy/service/PetServiceTest.kt
+++ src/test/kotlin/com/hibob/academy/service/PetServiceTest.kt
@@ -1,5 +1,6 @@
 package com.hibob.academy.service
 
+import com.hibob.academy.dao.OwnerData
 import com.hibob.academy.dao.PetDao
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.assertThrows
 import com.hibob.academy.dao.PetData
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.verify
+import org.mockito.kotlin.doNothing
 import org.mockito.kotlin.whenever
 
 class PetServiceTest {
@@ -49,67 +51,94 @@ class PetServiceTest {
         verify(petDao).petsByType(companyId, type)
     }
 
-//    @Test
-//    fun `should add pet successfully`() {
-//        val companyId = 1L
-//        val name = "Buddy"
-//        val type = PetType.DOG
-//        doNothing().`when`(petDao).insertPet(companyId, name, type.type)
-//
-//        petService.addPet(companyId, name, type)
-//
-//        verify(petDao).insertPet(companyId, name, type.type)
-//    }
-//
-//    @Test
-//    fun `should throw exception for blank pet name`() {
-//        val companyId = 1L
-//        val type = PetType.DOG
-//
-//        assertThrows<IllegalArgumentException> {
-//            petService.addPet(companyId, "", type)
-//        }
-//    }
-//
-//    @Test
-//    fun `should adopt pet successfully`() {
-//        val petId = 1
-//        val ownerId = 2L
-//        doNothing().`when`(petDao).adoptPet(petId, ownerId)
-//
-//        petService.adoptPet(petId, ownerId)
-//
-//        verify(petDao).adoptPet(petId, ownerId)
-//    }
-//
-//    @Test
-//    fun `should throw exception for invalid pet ID during adoption`() {
-//        val invalidPetId = -1
-//        val ownerId = 2L
-//
-//        assertThrows<IllegalArgumentException> {
-//            petService.adoptPet(invalidPetId, ownerId)
-//        }
-//    }
-//
-//    @Test
-//    fun `should get owner by pet ID successfully`() {
-//        val petId = 1
-//        val owner = OwnerData(1, "John Doe", 1L, "E123")
-//        whenever(petDao.getOwnerByPetId(petId)).thenReturn(owner)
-//
-//        val result = petService.getOwnerByPetId(petId)
-//
-//        assertEquals(owner, result)
-//        verify(petDao).getOwnerByPetId(petId)
-//    }
-//
-//    @Test
-//    fun `should throw exception for invalid pet ID when getting owner`() {
-//        val invalidPetId = -1
-//
-//        assertThrows<IllegalArgumentException> {
-//            petService.getOwnerByPetId(invalidPetId)
-//        }
-//    }
+    @Test
+    fun `should throw exception for blank pet name`() {
+        val companyId = 1L
+        val type = PetType.DOG
+
+        val exception = assertThrows<IllegalArgumentException> {
+            petService.addPet(companyId, "", type)
+        }
+        assertEquals("Name cannot be blank", exception.message)
+    }
+
+    @Test
+    fun `should throw exception for invalid company ID`() {
+        val companyId = -1L
+        val type = PetType.DOG
+
+        val exception = assertThrows<IllegalArgumentException> {
+            petService.addPet(companyId, "Murphy", type)
+        }
+
+        assertEquals("Company ID must be greater than 0", exception.message)
+    }
+
+    @Test
+    fun `should add pet successfully`() {
+        val companyId = 1L
+        val name = "Murphy"
+        val type = PetType.DOG
+        doNothing().whenever(petDao).insertPet(companyId, name, type.type)
+        assertDoesNotThrow {
+            petService.addPet(companyId, name, type)
+        }
+        verify(petDao).insertPet(companyId, name, type.type)
+    }
+
+    @Test
+    fun `should throw exception for invalid pet ID during adoption`() {
+        val invalidPetId = -1
+        val ownerId = 2L
+
+        val exception = assertThrows<IllegalArgumentException> {
+            petService.adoptPet(invalidPetId, ownerId)
+        }
+
+        assertEquals("Pet ID must be greater than 0", exception.message)
+    }
+
+    @Test
+    fun `should throw exception for invalid owner ID during adoption`() {
+        val petId = 1
+        val ownerId = -2L
+
+        val exception = assertThrows<IllegalArgumentException> {
+            petService.adoptPet(petId, ownerId)
+        }
+
+        assertEquals("Owner ID must be greater than 0", exception.message)
+    }
+
+        @Test
+    fun `should adopt pet successfully`() {
+        val petId = 1
+        val ownerId = 2L
+        doNothing().`when`(petDao).adoptPet(petId, ownerId)
+
+        petService.adoptPet(petId, ownerId)
+
+        verify(petDao).adoptPet(petId, ownerId)
+    }
+
+    @Test
+    fun `should throw exception for invalid pet ID when getting owner`() {
+        val invalidPetId = -1
+
+        assertThrows<IllegalArgumentException> {
+            petService.getOwnerByPetId(invalidPetId)
+        }
+    }
+
+        @Test
+    fun `should get owner by pet ID successfully`() {
+        val petId = 1
+        val owner = OwnerData(1, "Gilad", 1L, "1")
+        whenever(petDao.getOwnerByPetId(petId)).thenReturn(owner)
+
+        val result = petService.getOwnerByPetId(petId)
+
+        assertEquals(owner, result)
+        verify(petDao).getOwnerByPetId(petId)
+    }
 }
