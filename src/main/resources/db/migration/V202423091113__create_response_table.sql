295b6ac0ac5842d52741eed75e3e2d4601f34edf
Gilad Rozner <118189048+gilRoz23@users.noreply.github.com>
2024-09-23 12:28:29 +0300

    migrations building (#31)
    * migrations building
    
    * column names changing
    
    * indexing adding
    defaults adding
    
    ---------
    
    Co-authored-by: Gilad Rozner <gilad.rozner@hibob.io>

---
+++ src/main/resources/db/migration/V202423091113__create_response_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS response
+(
+    id BIGSERIAL PRIMARY KEY,
+    company_id BIGINT NOT NULL,
+    feedback_id BIGINT NOT NULL,
+    content TEXT NOT NULL,
+    responser_id BIGINT,
+    time_of_responsing TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX idx_response_company_id ON response (company_id);
+CREATE INDEX idx_response_feedback_id ON response (feedback_id);
