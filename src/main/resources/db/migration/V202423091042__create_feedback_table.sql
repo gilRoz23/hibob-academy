c88d4adc5336c57474f7440870980ccde3b592c1
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-23 11:26:52 +0300

    migrations building

---
+++ src/main/resources/db/migration/V202423091042__create_feedback_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS feedback
+(
+    id BIGSERIAL PRIMARY KEY,
+    company_id BIGINT NOT NULL,
+    feedback TEXT NOT NULL,
+    is_anonymous BOOLEAN NOT NULL,
+    status BOOLEAN NOT NULL,
+    feedback_provider_id BIGINT,
+    department VARCHAR(255),
+    time_of_submitting TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX idx_feedback_company_id ON feedback (company_id);
