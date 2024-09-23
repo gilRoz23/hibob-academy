8717f95a00627a4f7cdecd1746202eeab3ca8592
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-23 12:03:23 +0300

    feedback jooq table init and dataclass init

--- src/main/kotlin/com/hibob/academy/feedbacks_system/dao/tables.kt
+++ src/main/kotlin/com/hibob/academy/feedbacks_system/dao/tables.kt
@@ -1,47 +1,20 @@
 package com.hibob.academy.feedbacks_system.dao
+
 import com.hibob.academy.utils.JooqTable
+import com.sun.java.swing.ui.CommonUI.createTextField
 
-class PetTable(tableName : String = "pet") : JooqTable(tableName) {
-    val id = createIntField("id")
+class FeedbackTable(tableName : String = "feedback") : JooqTable(tableName) {
+    val id = createBigIntField("id")
     val companyId = createBigIntField("company_id")
-    val name = createVarcharField("name")
-    val type = createVarcharField("type")
-    val dateOfArrival = createDateField("date_of_arrival")
-    val ownerId = createBigIntField("owner_id")
+    val content = createTextField("content")
+    val isAnonymous = createBooleanField("is_anonymous")
+    val status = createBooleanField("status")
+    val feedbackProviderId = createBigIntField("feedback_provider_id")
+    val department = createVarcharField("department")
+    val timeOfSubmitting = createLocalDateTimeField("time_of_submitting")
 
 
     companion object{
-        val instance = PetTable()
-    }
-}
-
-class OwnerTable(tableName : String = "owner") : JooqTable(tableName) {
-
-    val id = createBigIntField("id")
-    val name = createVarcharField("name")
-    val companyId = createBigIntField("company_id")
-    val employeeId = createVarcharField("employee_id")
-
-    companion object{
-        val instance = OwnerTable()
-    }
-}
-
-class VaccineTable(tableName : String = "vaccine") : JooqTable(tableName) {
-    val id = createBigIntField("id")
-    val name = createVarcharField("name")
-
-    companion object{
-        val instance = VaccineTable()
-    }
-}
-
-class VaccineToPetTable(tableName : String = "vaccine_to_pet") : JooqTable(tableName) {
-    val id = createBigIntField("id")
-    val petId = createIntField("pet_id")
-    val vaccinationDate = createDateField("vaccination_date")
-
-    companion object{
-        val instance = VaccineToPetTable()
+        val instance = FeedbackTable()
     }
 }
