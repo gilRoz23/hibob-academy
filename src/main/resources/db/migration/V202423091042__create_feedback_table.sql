dbb4d4fe790b4a2fbb65ef5324cc3b5963acd0cf
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-23 12:26:47 +0300

    indexing adding defaults adding

--- src/main/resources/db/migration/V202423091042__create_feedback_table.sql
+++ src/main/resources/db/migration/V202423091042__create_feedback_table.sql
@@ -3,10 +3,12 @@ CREATE TABLE IF NOT EXISTS feedback
     id BIGSERIAL PRIMARY KEY,
     company_id BIGINT NOT NULL,
     content TEXT NOT NULL,
-    is_anonymous BOOLEAN NOT NULL,
-    status BOOLEAN NOT NULL,
+    is_anonymous BOOLEAN NOT NULL DEFAULT FALSE,
+    status BOOLEAN NOT NULL DEFAULT FALSE,
     feedback_provider_id BIGINT,
     department VARCHAR(255),
     time_of_submitting TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 CREATE INDEX idx_feedback_company_id ON feedback (company_id);
+CREATE INDEX idx_feedback_department ON feedback (department);
+CREATE INDEX idx_feedback_time ON feedback (time_of_submitting);
