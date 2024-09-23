8e3b981268477fafadd3c98a3e827371d49a6d70
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-23 20:30:45 +0300

    company Service

--- src/main/resources/db/migration/V202423092009__create_employee_table.sql
+++ src/main/resources/db/migration/V202423092009__create_employee_table.sql
@@ -3,6 +3,6 @@ CREATE TABLE employee (
                            id BIGSERIAL PRIMARY KEY,
                            first_name VARCHAR(255) NOT NULL,
                            last_name VARCHAR(255) NOT NULL,
-                           role VARCHAR(50),
+                           role VARCHAR(50) NOT NULL,
                            company_id BIGINT REFERENCES company(id)
 );
