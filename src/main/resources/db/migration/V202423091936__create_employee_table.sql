2df2606d8fd9f77a75ec13d4fd0d55b786f57a7a
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-23 19:45:58 +0300

    rebasing

---
+++ src/main/resources/db/migration/V202423091936__create_employee_table.sql
@@ -0,0 +1,8 @@
+-- Create employee table
+CREATE TABLE employee (
+                           id BIGSERIAL PRIMARY KEY,
+                           first_name VARCHAR(255) NOT NULL,
+                           last_name VARCHAR(255) NOT NULL,
+                           role VARCHAR(50),
+                           company_id BIGINT REFERENCES company(id)
+);
