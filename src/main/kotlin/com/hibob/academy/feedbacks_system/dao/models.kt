cc789363bc29256e4a05c5dd76e0e1cd57645b12
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-23 12:03:23 +0300

    feedback jooq table init and dataclass init

--- src/main/kotlin/com/hibob/academy/feedbacks_system/dao/models.kt
+++ src/main/kotlin/com/hibob/academy/feedbacks_system/dao/models.kt
@@ -1,27 +1,27 @@
 package com.hibob.academy.feedbacks_system.dao
-import java.time.LocalDate
+
+import java.time.LocalDateTime
+
+enum class Department {
+    HR,
+    IT,
+    SALES,
+    MARKETING,
+    FINANCE,
+    OPERATIONS,
+    RESEARCH_AND_DEVELOPMENT,
+    CUSTOMER_SERVICE,
+    LEGAL
+}
+
 
 data class FeedbackData(
     val id: Long,
     val company_id: Long,
-    val fee
+    val content: String,
+    val isAnonymous: Boolean,
+    val status: Boolean,
+    val feedbackProviderId: Long?,
+    val department: Department?,
+    val timeOfSubmitting: LocalDateTime
 )
-
-data class OwnerData(
-    val id: Long,
-    val name: String,
-    val companyId: Long,
-    val employeeId: String
-)
-
-data class VaccineData(
-    val id: Long,
-    val name: String
-)
-
-data class VaccineToPet
-    (
-            val id: Long,
-            val petId: Int,
-            val vaccintionDate: LocalDate
-            )
