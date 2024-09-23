7be78b08912602e10b4b3c6ce9c8b0651108860f
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-23 11:45:19 +0300

    column names changing

---
+++ src/main/kotlin/com/hibob/academy/feedbacks_system/dao/tables.kt
@@ -0,0 +1,47 @@
+package com.hibob.academy.feedbacks_system.dao
+import com.hibob.academy.utils.JooqTable
+
+class PetTable(tableName : String = "pet") : JooqTable(tableName) {
+    val id = createIntField("id")
+    val companyId = createBigIntField("company_id")
+    val name = createVarcharField("name")
+    val type = createVarcharField("type")
+    val dateOfArrival = createDateField("date_of_arrival")
+    val ownerId = createBigIntField("owner_id")
+
+
+    companion object{
+        val instance = PetTable()
+    }
+}
+
+class OwnerTable(tableName : String = "owner") : JooqTable(tableName) {
+
+    val id = createBigIntField("id")
+    val name = createVarcharField("name")
+    val companyId = createBigIntField("company_id")
+    val employeeId = createVarcharField("employee_id")
+
+    companion object{
+        val instance = OwnerTable()
+    }
+}
+
+class VaccineTable(tableName : String = "vaccine") : JooqTable(tableName) {
+    val id = createBigIntField("id")
+    val name = createVarcharField("name")
+
+    companion object{
+        val instance = VaccineTable()
+    }
+}
+
+class VaccineToPetTable(tableName : String = "vaccine_to_pet") : JooqTable(tableName) {
+    val id = createBigIntField("id")
+    val petId = createIntField("pet_id")
+    val vaccinationDate = createDateField("vaccination_date")
+
+    companion object{
+        val instance = VaccineToPetTable()
+    }
+}
