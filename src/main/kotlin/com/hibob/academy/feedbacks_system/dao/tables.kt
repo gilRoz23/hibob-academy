9b186cfb2ddeea921273ba341fa8060dd7ec16a1
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-23 13:36:16 +0300

    converting content field to varchar

--- src/main/kotlin/com/hibob/academy/feedbacks_system/dao/tables.kt
+++ src/main/kotlin/com/hibob/academy/feedbacks_system/dao/tables.kt
@@ -1,12 +1,11 @@
 package com.hibob.academy.feedbacks_system.dao
 
 import com.hibob.academy.utils.JooqTable
-import com.sun.java.swing.ui.CommonUI.createTextField
 
 class FeedbackTable(tableName : String = "feedback") : JooqTable(tableName) {
     val id = createBigIntField("id")
     val companyId = createBigIntField("company_id")
-    val content = createTextField("content")
+    val content = createVarcharField("content")
     val isAnonymous = createBooleanField("is_anonymous")
     val status = createBooleanField("status")
     val feedbackProviderId = createBigIntField("feedback_provider_id")
