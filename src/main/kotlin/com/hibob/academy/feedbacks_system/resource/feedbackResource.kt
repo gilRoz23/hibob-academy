3113680479debfd5e4ef6fb950686e8ef2fc25a2
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-24 18:54:42 +0300

    feedback resource done

--- src/main/kotlin/com/hibob/academy/feedbacks_system/resource/feedbackResource.kt
+++ src/main/kotlin/com/hibob/academy/feedbacks_system/resource/feedbackResource.kt
@@ -1,5 +1,6 @@
 package com.hibob.academy.feedbacks_system.resource
 
+import com.hibob.academy.feedbacks_system.Department
 import com.hibob.academy.feedbacks_system.service.FeedbackService
 import jakarta.ws.rs.*
 import jakarta.ws.rs.container.ContainerRequestContext
@@ -7,7 +8,6 @@ import jakarta.ws.rs.core.Context
 import jakarta.ws.rs.core.MediaType
 import jakarta.ws.rs.core.Response
 import org.springframework.stereotype.Controller
-
 
 @Controller
 @Path("/api/v1/employee-feedback")
@@ -17,14 +17,34 @@ class FeedbackResource(private val feedbackService: FeedbackService) {
 
     @POST
     @Path("/submit-feedback")
-    fun submitFeedback(@Context requestContext: ContainerRequestContext): Response {
-        val firstname = requestContext.getProperty("firstname") as String
-        val lastname = requestContext.getProperty("lastname") as String
-        val companyId = requestContext.getProperty("companyId") as String
-        val employeeId = requestContext.getProperty("employeeId") as String
-        val role = requestContext.getProperty("role") as String
-        feedbackService.insertFeedback(companyId, content, isAnonymous, feedbackProvider, department)
+    fun submitFeedback(
+        @Context requestContext: ContainerRequestContext,
+        feedbackRequest: FeedbackRequest
+    ): Response {
+        val feedbackProvider: Long? = if (feedbackRequest.isAnonymous) {
+            null
+        } else {
+            requestContext.getProperty("employeeId")?.toString()?.toLongOrNull()
+        }
+
+        val companyId = requestContext.getProperty("companyId")?.toString()?.toLongOrNull()
+            ?: return Response.status(Response.Status.BAD_REQUEST)
+                .entity("couldn't convert companyId").build()
+
+        feedbackService.insertFeedback(
+            companyId,
+            feedbackRequest.content,
+            feedbackRequest.isAnonymous,
+            feedbackProvider,
+            feedbackRequest.department
+        )
 
         return Response.ok("Feedback submitted successfully").build()
     }
 }
+
+data class FeedbackRequest(
+    val content: String,
+    val isAnonymous: Boolean,
+    val department: Department
+)
