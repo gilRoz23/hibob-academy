bd36afa4594ec689e20425c2093ad7b6dbb56edc
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-24 17:46:13 +0300

    bla

--- src/main/kotlin/com/hibob/academy/feedbacks_system/resource/feedbackResource.kt
+++ src/main/kotlin/com/hibob/academy/feedbacks_system/resource/feedbackResource.kt
@@ -1,4 +1,6 @@
 package com.hibob.academy.feedbacks_system.resource
+
+import com.hibob.academy.feedbacks_system.service.FeedbackService
 import jakarta.ws.rs.*
 import jakarta.ws.rs.core.MediaType
 import jakarta.ws.rs.core.Response
@@ -9,54 +11,20 @@ import org.springframework.stereotype.Controller
 @Path("/api/v1/employee-feedback")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-class FeedbackResource() {
+class FeedbackResource(private val feedbackService: FeedbackService) {
 
-    //    temporary database simulation
-    private val allPets: MutableList<Pet> = mutableListOf()
-
-    //
     @GET
-    @Path("/{petId}")
-    fun getPet(@PathParam("petId") petId: Int): Response {
-        val pet = allPets.find { pet: Pet -> pet.id == petId }
-        pet?.let {
-            return Response.ok(pet).build()
-        } ?: return Response.status(Response.Status.NOT_FOUND).entity("pet not found").build()
-    }
+    @Path("/submit-feedback")
+    fun submitFeedback(): Response {
 
 
-    @POST
-    fun postPet(pet: Pet): Response {
-        val firstname : String = pet.name.split(" ").first()
-        val lastname = pet.name.split(" ").drop(1).joinToString(" ")
-        allPets.add(pet.copy(id = pet.id, name = pet.name, firstname = firstname, lastname = lastname ,type = pet.type, companyId = pet.companyId, dateOfArrival = pet.dateOfArrival))
-        return Response.status(Response.Status.CREATED).entity(Response.Status.CREATED).build()
-    }
-
-    @PUT
-    @Path("/{petId}")
-    fun putPet(@PathParam("petId") petId: Int, pet: Pet): Response {
-        val index = allPets.indexOfFirst { pet -> pet.id == petId }
-        if (index >= 0) {
-            val petToUpdate = allPets.removeAt(index).copy(id = pet.id, name = pet.name, type = pet.type, companyId = pet.companyId, dateOfArrival = pet.dateOfArrival)
-            allPets.add(petToUpdate)
-            return Response.ok(petToUpdate).build()
-        } else {
-            return Response.status(Response.Status.NOT_FOUND).entity("pet not found").build()
-        }
-    }
-
-    @DELETE
-    @Path("/{petId}")
-    fun deletePet(@PathParam("petId") petId: Int, pet: Pet): Response {
-        val index = allPets.indexOfFirst { pet -> pet.id == petId }
-        if (index >= 0) {
-            allPets.removeAt(index)
-            return Response.ok(pet).build()
-        } else {
-            return Response.status(Response.Status.NOT_FOUND).entity("pet with id ${petId} not found").build()
-        }
+//        return try {
+//            val petsList = petService.getPetsByType(companyId, petType)
+//            Response.ok(petsList).build()
+//        } catch (e: IllegalArgumentException) {
+//            Response.status(Response.Status.BAD_REQUEST).entity(e.message).build()
+//        } catch (e: NoContentException) {
+//            Response.noContent().build()
+//        }
     }
 }
-
-//data class ExampleResponse(val data: Example)
