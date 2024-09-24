e888bc193d7c7eca8cd45858fd647623b7a6d1a6
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-24 15:55:53 +0300

    company and employees migrations

--- src/main/resources/db/migration/V202424091524__insert_companys.sql
+++ src/main/resources/db/migration/V202424091524__insert_companys.sql
@@ -1,9 +1,4 @@
--- Create employee table
-CREATE TABLE employee
-(
-    id         BIGSERIAL PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name  VARCHAR(255) NOT NULL,
-    role       VARCHAR(50)  NOT NULL,
-    company_id BIGINT REFERENCES company (id)
-);
+-- Insert companies (only if they don't already exist)
+INSERT INTO company (name)
+VALUES ('Friends'),
+       ('Seinfeld');
