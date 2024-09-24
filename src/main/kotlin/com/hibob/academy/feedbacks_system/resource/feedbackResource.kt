aba2521d37704cd2e2d6261d1b7ce6e9e0f375cc
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-24 18:04:15 +0300

    bla

--- src/main/kotlin/com/hibob/academy/feedbacks_system/resource/feedbackResource.kt
+++ src/main/kotlin/com/hibob/academy/feedbacks_system/resource/feedbackResource.kt
@@ -2,6 +2,8 @@ package com.hibob.academy.feedbacks_system.resource
 
 import com.hibob.academy.feedbacks_system.service.FeedbackService
 import jakarta.ws.rs.*
+import jakarta.ws.rs.container.ContainerRequestContext
+import jakarta.ws.rs.core.Context
 import jakarta.ws.rs.core.MediaType
 import jakarta.ws.rs.core.Response
 import org.springframework.stereotype.Controller
@@ -13,18 +15,16 @@ import org.springframework.stereotype.Controller
 @Consumes(MediaType.APPLICATION_JSON)
 class FeedbackResource(private val feedbackService: FeedbackService) {
 
-    @GET
+    @POST
     @Path("/submit-feedback")
-    fun submitFeedback(): Response {
+    fun submitFeedback(@Context requestContext: ContainerRequestContext): Response {
+        val firstname = requestContext.getProperty("firstname") as String
+        val lastname = requestContext.getProperty("lastname") as String
+        val companyId = requestContext.getProperty("companyId") as String
+        val employeeId = requestContext.getProperty("employeeId") as String
+        val role = requestContext.getProperty("role") as String
+        feedbackService.insertFeedback(companyId, content, isAnonymous, feedbackProvider, department)
 
-
-//        return try {
-//            val petsList = petService.getPetsByType(companyId, petType)
-//            Response.ok(petsList).build()
-//        } catch (e: IllegalArgumentException) {
-//            Response.status(Response.Status.BAD_REQUEST).entity(e.message).build()
-//        } catch (e: NoContentException) {
-//            Response.noContent().build()
-//        }
+        return Response.ok("Feedback submitted successfully").build()
     }
 }
