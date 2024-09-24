4551bc150831cb55591d9ea3e57f3e735dbfb988
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-24 09:30:19 +0300

    company resource

--- src/main/resources/db/migration/V202423091936__create_company_table.sql
+++ src/main/resources/db/migration/V202423091936__create_company_table.sql
@@ -1,5 +1,6 @@
 -- Create company table
-CREATE TABLE company (
-                         id BIGSERIAL PRIMARY KEY,
-                         name VARCHAR(255) NOT NULL
+CREATE TABLE company
+(
+    id   BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
 );
