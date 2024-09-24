394dad9af2a8ed021a2656a6c094712f0f1d0a2b
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-24 15:55:15 +0300

    company and employees migrations

---
+++ src/main/resources/db/migration/V202424091524__insert_companys.sql
@@ -0,0 +1,9 @@
+-- Create employee table
+CREATE TABLE employee
+(
+    id         BIGSERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name  VARCHAR(255) NOT NULL,
+    role       VARCHAR(50)  NOT NULL,
+    company_id BIGINT REFERENCES company (id)
+);
