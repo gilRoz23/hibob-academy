0462ea3cbc889dcc5f5748759370acf2e36748ab
Gilad Rozner <118189048+gilRoz23@users.noreply.github.com>
2024-09-25 11:04:58 +0300

    Gilad/feedback system/feedback resource insert feedback (#43)
    * company service deleting
    
    * bla
    
    * bla
    
    * feedback resource done
    
    * commit
    
    * commit
    
    ---------
    
    Co-authored-by: Gilad Rozner <gilad.rozner@hibob.io>

---
+++ src/main/kotlin/com/hibob/academy/feedbacks_system/resource/feedbackResource.kt
@@ -0,0 +1,52 @@
+package com.hibob.academy.feedbacks_system.resource
+
+import com.hibob.academy.feedbacks_system.Department
+import com.hibob.academy.feedbacks_system.service.FeedbackService
+import jakarta.ws.rs.*
+import jakarta.ws.rs.container.ContainerRequestContext
+import jakarta.ws.rs.core.Context
+import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.Response
+import org.springframework.stereotype.Controller
+
+@Controller
+@Path("/api/v1/employee-feedback")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+class FeedbackResource(private val feedbackService: FeedbackService) {
+
+    @POST
+    @Path("/submit-feedback")
+    fun submitFeedback(@Context requestContext: ContainerRequestContext, feedbackRequest: FeedbackRequest): Response {
+        val feedbackProvider: Long? = if (feedbackRequest.isAnonymous) {
+            null
+        } else {
+            extractPropertyAsLong(requestContext, "employeeId")
+        }
+
+        val companyId = extractPropertyAsLong(requestContext, "companyId")
+            ?: return Response.status(Response.Status.BAD_REQUEST)
+                .entity("couldn't convert companyId").build()
+
+        feedbackService.insertFeedback(
+            companyId,
+            feedbackRequest.content,
+            feedbackRequest.isAnonymous,
+            feedbackProvider,
+            feedbackRequest.department
+        )
+
+        return Response.status(Response.Status.CREATED).entity("Feedback submitted successfully").build()
+    }
+
+    // Private function to extract properties as Long
+    private fun extractPropertyAsLong(requestContext: ContainerRequestContext, propertyName: String): Long? {
+        return requestContext.getProperty(propertyName)?.toString()?.toLongOrNull()
+    }
+}
+
+data class FeedbackRequest(
+    val content: String,
+    val isAnonymous: Boolean,
+    val department: Department
+)
