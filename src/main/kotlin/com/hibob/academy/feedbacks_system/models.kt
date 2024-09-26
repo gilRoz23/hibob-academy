8e6c02eb3857c3b8937a41497c673c994d984beb
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-26 16:48:08 +0300

    fixing pr

--- src/main/kotlin/com/hibob/academy/feedbacks_system/models.kt
+++ src/main/kotlin/com/hibob/academy/feedbacks_system/models.kt
@@ -42,6 +42,24 @@ data class CompanyData(
 )
 
 
+data class FeedbackFilter(
+    val companyId: Long,
+    val isAnonymous: Boolean? = null,
+    val status: Boolean? = null,
+    val feedbackProviderId: Long? = null,
+    val department: Department? = null,
+    val timeOfSubmitting: LocalDateTime? = null
+)
+
+data class UserFeedbackFilter(
+    val isAnonymous: Boolean? = null,
+    val status: Boolean? = null,
+    val feedbackProviderId: Long? = null,
+    val department: Department? = null,
+    val timeOfSubmitting: LocalDateTime? = null
+)
+
+
 data class FeedbackRequest(
     val content: String,
     val isAnonymous: Boolean,
