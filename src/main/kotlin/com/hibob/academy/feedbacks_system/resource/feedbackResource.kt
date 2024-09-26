1baeca51933e26ca48eaa8e4531a5b0549b710a6
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-26 16:16:25 +0300

    review fixing

--- src/main/kotlin/com/hibob/academy/feedbacks_system/resource/feedbackResource.kt
+++ src/main/kotlin/com/hibob/academy/feedbacks_system/resource/feedbackResource.kt
@@ -4,6 +4,7 @@ import PermissionService
 import com.hibob.academy.feedbacks_system.FeedbackRequest
 import com.hibob.academy.feedbacks_system.Role
 import com.hibob.academy.feedbacks_system.service.FeedbackService
+import com.hibob.academy.feedbacks_system.UserFeedbackFilter
 import jakarta.ws.rs.*
 import jakarta.ws.rs.container.ContainerRequestContext
 import jakarta.ws.rs.core.Context
@@ -55,6 +56,20 @@ class FeedbackResource(
             permissionService.validatePermission(role, listOf(Role.HR, Role.ADMIN))
         }?.let {
             return Response.ok(feedbackService.getAllCompanyFeedbacks(it)).build()
+        } ?: return Response.status(Response.Status.FORBIDDEN).entity("Access denied").build()
+    }
+
+    @GET
+    @Path("/filter-feedbacks")
+    fun filterFeedbacks(@Context requestContext: ContainerRequestContext, userFeedbackFilter: UserFeedbackFilter): Response {
+        val permissionService = PermissionService()
+        val role = permissionService.extractPropertyAsString(requestContext, "role") ?: ""
+        val companyId = permissionService.extractPropertyAsLong(requestContext, "companyId")
+
+        companyId?.takeIf {
+            permissionService.validatePermission(role, listOf(Role.HR, Role.ADMIN))
+        }?.let {
+            return Response.ok(feedbackService.filterFeedbacks(it, userFeedbackFilter)).build()
         } ?: return Response.status(Response.Status.FORBIDDEN).entity("Access denied").build()
     }
 }
