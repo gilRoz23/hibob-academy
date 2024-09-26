076315fb5eeb3e58bab950b109f36372452e7f51
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-26 18:13:11 +0300

    response service

---
+++ src/main/kotlin/com/hibob/academy/feedbacks_system/resource/responseResource.kt
@@ -0,0 +1,77 @@
+package com.hibob.academy.feedbacks_system.resource
+
+import PermissionService
+import com.hibob.academy.feedbacks_system.FeedbackRequest
+import com.hibob.academy.feedbacks_system.ResponseRequest
+import com.hibob.academy.feedbacks_system.Role
+import com.hibob.academy.feedbacks_system.service.FeedbackService
+import com.hibob.academy.feedbacks_system.UserFeedbackFilter
+import com.hibob.academy.feedbacks_system.service.ResponseService
+import jakarta.ws.rs.*
+import jakarta.ws.rs.container.ContainerRequestContext
+import jakarta.ws.rs.core.Context
+import jakarta.ws.rs.core.MediaType
+import jakarta.ws.rs.core.Response
+import org.springframework.stereotype.Controller
+
+@Controller
+@Path("/api/v1/employee-feedback")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+class ResponseResource(
+    private val responseService: ResponseService
+) {
+
+    @POST
+    @Path("/respond")
+    fun submitFeedback(@Context requestContext: ContainerRequestContext, responseRequest: ResponseRequest): Response {
+        val permissionService = PermissionService()
+        val feedbackProvider: Long? = if (feedbackRequest.isAnonymous) {
+            null
+        } else {
+            permissionService.extractPropertyAsLong(requestContext, "employeeId")
+        }
+
+        val companyId = permissionService.extractPropertyAsLong(requestContext, "companyId")
+            ?: return Response.status(Response.Status.BAD_REQUEST)
+                .entity("couldn't convert companyId").build()
+
+        feedbackService.insertFeedback(
+            companyId,
+            feedbackRequest.content,
+            feedbackRequest.isAnonymous,
+            feedbackProvider,
+            feedbackRequest.department
+        )
+
+        return Response.status(Response.Status.CREATED).entity("Feedback submitted successfully").build()
+    }
+
+    @GET
+    @Path("/get-all-feedbacks")
+    fun getAllFeedbacks(@Context requestContext: ContainerRequestContext): Response {
+        val permissionService = PermissionService()
+        val role = permissionService.extractPropertyAsString(requestContext, "role") ?: ""
+        val companyId = permissionService.extractPropertyAsLong(requestContext, "companyId")
+
+        companyId?.takeIf {
+            permissionService.validatePermission(role, listOf(Role.HR, Role.ADMIN))
+        }?.let {
+            return Response.ok(feedbackService.getAllCompanyFeedbacks(it)).build()
+        } ?: return Response.status(Response.Status.FORBIDDEN).entity("Access denied").build()
+    }
+
+    @GET
+    @Path("/filter-feedbacks")
+    fun filterFeedbacks(@Context requestContext: ContainerRequestContext, userFeedbackFilter: UserFeedbackFilter): Response {
+        val permissionService = PermissionService()
+        val role = permissionService.extractPropertyAsString(requestContext, "role") ?: ""
+        val companyId = permissionService.extractPropertyAsLong(requestContext, "companyId")
+
+        companyId?.takeIf {
+            permissionService.validatePermission(role, listOf(Role.HR, Role.ADMIN))
+        }?.let {
+            return Response.ok(feedbackService.filterFeedbacks(it, userFeedbackFilter)).build()
+        } ?: return Response.status(Response.Status.FORBIDDEN).entity("Access denied").build()
+    }
+}
