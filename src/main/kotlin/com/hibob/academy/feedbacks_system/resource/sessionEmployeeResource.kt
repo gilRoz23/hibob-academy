42bb76d57d49bf57b34cadb40fd7a45d6023a003
Gilad Rozner <gilad.rozner@hibob.io>
2024-09-26 16:48:57 +0300

    fixing pr

--- src/main/kotlin/com/hibob/academy/feedbacks_system/resource/sessionEmployeeResource.kt
+++ src/main/kotlin/com/hibob/academy/feedbacks_system/resource/sessionEmployeeResource.kt
@@ -4,7 +4,6 @@ import com.hibob.academy.feedbacks_system.JWTDetails
 import com.hibob.academy.feedbacks_system.FeedbackData
 import com.hibob.academy.feedbacks_system.service.SessionEmployeeService
 import jakarta.ws.rs.Consumes
-import com.hibob.academy.feedbacks_system.UserFeedbackFilter
 import jakarta.ws.rs.POST
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
